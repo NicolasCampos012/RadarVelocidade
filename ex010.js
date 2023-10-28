8ccc336211962eeadb30221457962dc2f126da94
Nicolas Campos Francisco <112672297+NicolasCampos012@users.noreply.github.com>
2023-10-28 00:58:54 -0300

    Add files via upload

---
+++ ex010.js
@@ -0,0 +1,6 @@
+var vel = 45
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60)
+console.log("Você está acima do limite de velocidade")
+else (vel < 60)
+console.log('Boa viagem')
